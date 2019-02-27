5617c57ad42c0a794513a179fd498fc8a1b93729
HzKharel <43970402+HzKharel@users.noreply.github.com>
2019-02-27 21:57:52 +0000

    Autokey fully implemented, web page updated to reflect it.

--- scripts/Autokey.js
+++ scripts/Autokey.js
@@ -1,82 +1,80 @@
-
-autokey("wswpgpoisheswowjlxmvswg", "polestar",true);
-
-function autokey(plain_text, key , decrypt) {
+function autokey(plain_text, key, decrypt) {
 
     var key_stream = key.toLowerCase();
     var plain_text_stream = "";
     var output = "";
 
-    //removing punctuation and spaces from plain text to build a key stream
-    for(var i = 0; i < plain_text.length; i++){
-        if (plain_text[i].toUpperCase() !== plain_text[i].toLowerCase()) {
-            plain_text_stream += plain_text[i].toLowerCase();
+    if (!decrypt) {
+        //removing punctuation and spaces from plain text to build a key stream
+        for (var i = 0; i < plain_text.length; i++) {
+            if (plain_text[i].toUpperCase() !== plain_text[i].toLowerCase()) {
+                plain_text_stream += plain_text[i].toLowerCase();
+            }
+        }
+
+        //building the key stream
+        for (var i = 0; key_stream.length < plain_text_stream.length; i++) {
+            key_stream += plain_text_stream[i];
         }
     }
 
-    //building the key stream
-    for(var i = 0; key_stream.length < plain_text_stream.length; i++){
-        key_stream += plain_text_stream[i];
-    }
 
     var key_stream_index = 0;
     //now encrypting the message
-    for(var i = 0; i <plain_text.length; i++){
+    for (var i = 0; i < plain_text.length; i++) {
 
         if (plain_text[i].toUpperCase() !== plain_text[i].toLowerCase()) {
 
             var plain_text_code = plain_text[i].charCodeAt(0);
             var key_code = (key_stream[key_stream_index].charCodeAt(0));
-            console.log(plain_text_code + "   "+key_code);
 
-            if(plain_text[i].toUpperCase() === plain_text[i]){
+            if (plain_text[i].toUpperCase() === plain_text[i]) {
 
-                if(decrypt){
-                    var crypt_code = ((plain_text_code-65)-(key_code-97))%26;
+                if (decrypt) {
+                    var crypt_code = ((plain_text_code - 65) - (key_code - 97)) % 26;
 
                     if (crypt_code < 0) {
                         crypt_code = 26 + crypt_code;
                     }
-                  //  console.log(crypt_code);
-                    output += String.fromCharCode(crypt_code+65);
+                    //  console.log(crypt_code);
+                    var letter = String.fromCharCode(crypt_code + 65);
+                    output += letter;
+                    key_stream += letter.toLowerCase();
                 }
 
                 else {
-                    var crypt_code = ((plain_text_code-65)+(key_code-97))%26;
-                    output += String.fromCharCode(crypt_code+65);
+                    var crypt_code = ((plain_text_code - 65) + (key_code - 97)) % 26;
+                    output += String.fromCharCode(crypt_code + 65);
                 }
             }
-            else{
+            else {
 
-                if(decrypt){
-                    var crypt_code = ((plain_text_code-97)-(key_code-97))%26;
-
+                if (decrypt) {
+                    var crypt_code = ((plain_text_code - 97) - (key_code - 97)) % 26;
 
                     if (crypt_code < 0) {
                         crypt_code = 26 + crypt_code;
                     }
+                    var letter = String.fromCharCode(crypt_code + 97);
 
-                    output += String.fromCharCode(crypt_code+97);
+                    output += letter;
+                    key_stream += letter.toLowerCase();
                 }
 
                 else {
-                    var crypt_code = ((plain_text_code-97)+(key_code-97))%26;
-                    output += String.fromCharCode(crypt_code+97);
+                    var crypt_code = ((plain_text_code - 97) + (key_code - 97)) % 26;
+                    output += String.fromCharCode(crypt_code + 97);
                 }
-
             }
 
             key_stream_index++;
         }
 
-        else{
+        else {
             output += plain_text[i];
         }
 
     }
 
-
-    console.log(plain_text_stream);
-    console.log(key_stream);
-    console.log(output);
+    return output;
 }
--- scripts/main.js
+++ scripts/main.js
@@ -9,16 +9,19 @@ function encode(isDecrypt) {
         case "ROT 13 Cipher":
 
             output.value = rot13(plain_text);
-            output.style.display = "block";
             break;
 
         case "Vignere Cipher":
 
             output.value = vignere_Cipher(plain_text, key, isDecrypt);
-            output.style.display = "block";
             break;
 
+        case "Autokey Cipher":
+            output.value = autokey(plain_text, key, isDecrypt);
+            break;
     }
+
+    output.style.display = "block";
 }
 
 //////////////////ROT 13//////////////////////
@@ -117,5 +120,90 @@ function vignere_Cipher(input, key, decrypt) {
 
         }
     }
+    return output;
+}
+
+//////////////////Autokey Cipher//////////////////////
+//////////////////Autokey Cipher//////////////////////
+//////////////////Autokey Cipher//////////////////////
+
+function autokey(plain_text, key, decrypt) {
+
+    var key_stream = key.toLowerCase();
+    var plain_text_stream = "";
+    var output = "";
+
+    if (!decrypt) {
+        //removing punctuation and spaces from plain text to build a key stream
+        for (var i = 0; i < plain_text.length; i++) {
+            if (plain_text[i].toUpperCase() !== plain_text[i].toLowerCase()) {
+                plain_text_stream += plain_text[i].toLowerCase();
+            }
+        }
+
+        //building the key stream
+        for (var i = 0; key_stream.length < plain_text_stream.length; i++) {
+            key_stream += plain_text_stream[i];
+        }
+    }
+
+
+    var key_stream_index = 0;
+    //now encrypting the message
+    for (var i = 0; i < plain_text.length; i++) {
+
+        if (plain_text[i].toUpperCase() !== plain_text[i].toLowerCase()) {
+
+            var plain_text_code = plain_text[i].charCodeAt(0);
+            var key_code = (key_stream[key_stream_index].charCodeAt(0));
+
+            if (plain_text[i].toUpperCase() === plain_text[i]) {
+
+                if (decrypt) {
+                    var crypt_code = ((plain_text_code - 65) - (key_code - 97)) % 26;
+
+                    if (crypt_code < 0) {
+                        crypt_code = 26 + crypt_code;
+                    }
+                    //  console.log(crypt_code);
+                    var letter = String.fromCharCode(crypt_code + 65);
+                    output += letter;
+                    key_stream += letter.toLowerCase();
+                }
+
+                else {
+                    var crypt_code = ((plain_text_code - 65) + (key_code - 97)) % 26;
+                    output += String.fromCharCode(crypt_code + 65);
+                }
+            }
+            else {
+
+                if (decrypt) {
+                    var crypt_code = ((plain_text_code - 97) - (key_code - 97)) % 26;
+
+                    if (crypt_code < 0) {
+                        crypt_code = 26 + crypt_code;
+                    }
+                    var letter = String.fromCharCode(crypt_code + 97);
+
+                    output += letter;
+                    key_stream += letter.toLowerCase();
+                }
+
+                else {
+                    var crypt_code = ((plain_text_code - 97) + (key_code - 97)) % 26;
+                    output += String.fromCharCode(crypt_code + 97);
+                }
+            }
+
+            key_stream_index++;
+        }
+
+        else {
+            output += plain_text[i];
+        }
+
+    }
+
     return output;
 }
